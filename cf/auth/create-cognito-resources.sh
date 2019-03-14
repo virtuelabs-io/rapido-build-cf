bd3a00a79ab52bf6079bbcae6a32a004ec73fe66
Sangram Reddy <reddy.horcrux@gmail.com>
2019-03-14 18:19:22 +0000

    Cognito cloudformation templates + scripts

---
+++ cf/auth/create-cognito-resources.sh
@@ -0,0 +1,8 @@
+COGNITO_AUTH_NAME=rapido
+
+aws cloudformation create-stack \
+    --stack-name rapido-build-cognito-resources \
+    --template-body file://${PWD}/cf/auth/cognito-resources.yaml \
+    --parameters ParameterKey=AuthName,ParameterValue=$COGNITO_AUTH_NAME \
+    --capabilities CAPABILITY_IAM \
+    --profile rapido-devops
