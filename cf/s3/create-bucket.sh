c6f59dc8caa8500154e30293f13fcd23d6088453
Sangram Reddy <reddy.horcrux@gmail.com>
2019-03-31 18:50:55 +0100

    Added schema

--- cf/s3/create-bucket.sh
+++ cf/s3/create-bucket.sh
@@ -1,4 +1,4 @@
-BUCKET_PREFIX=rapido-build
+export BUCKET_PREFIX=rapido-build
 
 aws cloudformation create-stack \
     --stack-name $BUCKET_PREFIX-assets-bucket \
@@ -11,4 +11,11 @@ aws cloudformation create-stack \
     --stack-name $BUCKET_PREFIX-secrets-bucket \
     --template-body file://${PWD}/cf/s3/secrets-bucket.yaml \
     --parameters ParameterKey=SecretsBucketName,ParameterValue=$BUCKET_PREFIX-secrets-bucket \
+    --profile rapido-devops
+
+aws cloudformation create-stack \
+    --stack-name $BUCKET_PREFIX-schemas-bucket \
+    --template-body file://${PWD}/cf/s3/schemas-bucket.yaml \
+    --parameters ParameterKey=SchemasBucketName,ParameterValue=$BUCKET_PREFIX-schemas-bucket \
+    --capabilities CAPABILITY_IAM \
     --profile rapido-devops
