d89c4f7f239bec4f49550520767f4e65b3d1594b
Sangram Reddy <reddy.horcrux@gmail.com>
2019-08-09 00:26:24 +0100

    Added CF templates for route53 and hosting bucket

---
+++ cf/route53/deploy_hosted_zone.sh
@@ -0,0 +1,5 @@
+aws cloudformation create-stack \
+    --stack-name dev-rapido-build-dev-hosted-zone \
+    --template-body file://${PWD}/cf/route53/hosted-zone.yaml \
+    --parameters ParameterKey=DomainName,ParameterValue=dev.rapidobuild.com \
+    --profile rapido-devops
--- cf/s3/create-bucket.sh
+++ cf/s3/create-bucket.sh
@@ -1,4 +1,5 @@
 export BUCKET_PREFIX=rapido-build
+export DEV_RAPIDO_BUILD_S3_BUCKET=dev-rapido-build-website
 
 aws cloudformation create-stack \
     --stack-name $BUCKET_PREFIX-assets-bucket \
@@ -18,4 +19,10 @@ aws cloudformation create-stack \
     --template-body file://${PWD}/cf/s3/schemas-bucket.yaml \
     --parameters ParameterKey=SchemasBucketName,ParameterValue=$BUCKET_PREFIX-schemas-bucket \
     --capabilities CAPABILITY_IAM \
+    --profile rapido-devops
+
+aws cloudformation create-stack \
+    --stack-name dev-rapido-build-website \
+    --template-body file://${PWD}/cf/s3/website-bucket.yaml \
+    --parameters ParameterKey=BucketName,ParameterValue=$DEV_RAPIDO_BUILD_S3_BUCKET \
     --profile rapido-devops
