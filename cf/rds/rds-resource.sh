4e5af8701638dd1f6958cc1e3c80dd829c75ec23
Sangram Reddy <reddy.horcrux@gmail.com>
2020-02-21 23:24:32 +0000

    updated cloudformation templates

--- cf/rds/rds-resource.sh
+++ cf/rds/rds-resource.sh
@@ -1,14 +1,16 @@
 aws cloudformation create-stack \
     --stack-name rapido-build-$DB_NAME-database \
-    --template-body file://${PWD}/cf/rds/rds-test.yaml \
+    --template-body file://${PWD}/cf/rds/rds.yaml \
     --parameters ParameterKey=DBInstanceClass,ParameterValue=$DB_INSTANCE_CLASS \
                  ParameterKey=DBName,ParameterValue=$DB_NAME \
                  ParameterKey=AllocatedStorage,ParameterValue=$ALLOCATED_STORAGE \
-                 ParameterKey=Username,ParameterValue=$USERNAME \
+                 ParameterKey=Username,ParameterValue=$DB_USERNAME \
                  ParameterKey=Password,ParameterValue=$PASSWORD \
                  ParameterKey=VpcId,ParameterValue=$VPC_ID \
                  ParameterKey=Engine,ParameterValue=$ENGINE \
                  ParameterKey=EngineVersion,ParameterValue=$ENGINE_VERSION \
+                 ParameterKey=EnvironmentName,ParameterValue=$ENVIRONMENT_NAME \
+                 ParameterKey=Port,ParameterValue=$PORT \
+                 ParameterKey=PrivateSubnets,ParameterValue=\"subnet-036ec93f28aa1e035,subnet-0f708f6e7f34226a0\" \
     --capabilities CAPABILITY_IAM \
-    --profile rapido-devops
-
+    --profile DevECommerce
